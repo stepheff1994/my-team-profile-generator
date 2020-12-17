f54516cb75d7ee624a9d9a035b366132769835ff
Stephanie Freyler <stephaniefreylersantana@gmail.com>
2020-12-17 15:54:34 -0500

    made Employee files

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,20 @@
+//const jest = require("jest")
+const Employee = require("../lib/Employee.class.js");
+
+test("creates an employee object", () => {
+  const employee = new Employee("joe", 89, "joe@gmail.com");
+
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.id).toBe(89);
+  expect(employee.email).toBe("joe@gmail.com");
+});
+
+test("should create a name through the contructor argument", () => {
+  const employee = new Employee("joe", 89, "joe@gmail.com");
+  expect(employee.name).toBe("joe");
+});
+
+test("should get a name through getName()", () => {
+  const employee = new Employee("joe", 89, "joe@gmail.com");
+  expect(employee.getName()).toBe("joe");
+});
--- index.js
+++ index.js
@@ -1,131 +1,120 @@
-const inquirer = require("inquirer")
-const fs = require('fs')
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // fs.writeFile('./index.html', generatePage(name, github), err => {
 //     if (err) throw new Error(err);
-  
+
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 //   });
- 
+
 // array of questions for user
-const questions = [ 
-    {
-        type: "input",
-        name: "managerName",
-        message: "What is the manager's name?"
-    
-    },
-    {
-        type: "input",
-        name: "employeeId",
-        message: "What is the manager's employee ID?"
-    
-    },
-    {
-        type: "input",
-        name: "emailAdress",
-        message: "What is the manager's email adress?"
-    
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the manager's office number?"
-    
-    },
-    {
-        type: "list",
-        name: "menu",
-        message: "Who would you like to add?",
-        choices: ["Engineer", "Intern", "Finish building my team"]
-    
-    },
+const managerQuestions = [
+  {
+    type: "input",
+    name: "managerName",
+    message: "What is the manager's name?",
+  },
+  {
+    type: "input",
+    name: "employeeId",
+    message: "What is the manager's employee ID?",
+  },
+  {
+    type: "input",
+    name: "emailAdress",
+    message: "What is the manager's email adress?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the manager's office number?",
+  },
+];
 
+const addMember = [
+  {
+    type: "list",
+    name: "menu",
+    message: "Who would you like to add?",
+    choices: ["Engineer", "Intern", "Finish building my team"],
+  },
+];
 
-]
 const engineerQuestions = [
+  {
+    type: "input",
+    name: "engineerName",
+    message: "What is the engineer's name?",
+  },
+  {
+    type: "input",
+    name: "employeeId",
+    message: "What is the engineer's employee ID?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineer's github username?",
+  },
+  {
+    type: "input",
+    name: "emailAdress",
+    message: "What is the engineer's email adress?",
+  },
+];
 
-    {
-        type: "input",
-        name: "engineerName",
-        message: "What is the engineer's name?"
-    
-    },
-    {
-        type: "input",
-        name: "employeeId",
-        message: "What is the engineer's employee ID?"
-    
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is the engineer's github username?"
-    
-    },
-    {
-        type: "input",
-        name: "emailAdress",
-        message: "What is the engineer's email adress?"
-    
-    },
-    {
-        type: "list",
-        name: "menu",
-        message: "Who would you like to add?",
-        choices: ["Engineer", "Intern", "Finish building my team"]
-    
-    },
-
-]
 const internQuestions = [
-
-    {
-        type: "input",
-        name: "internName",
-        message: "What is the intern's name?"
-    
-    },
-    {
-        type: "input",
-        name: "employeeId",
-        message: "What is the intern's employee ID?"
-    
-    },
-    {
-        type: "input",
-        name: "internSchool",
-        message: "What is the name of the intern's school?"
-    
-    },
-    {
-        type: "input",
-        name: "emailAdress",
-        message: "What is the intern's email adress?"
-    
-    },
-    {
-        type: "list",
-        name: "menu",
-        message: "Who would you like to add?",
-        choices: ["Engineer", "Intern", "Finish building my team"]
-    
-    },
-
-]
+  {
+    type: "input",
+    name: "internName",
+    message: "What is the intern's name?",
+  },
+  {
+    type: "input",
+    name: "employeeId",
+    message: "What is the intern's employee ID?",
+  },
+  {
+    type: "input",
+    name: "internSchool",
+    message: "What is the name of the intern's school?",
+  },
+  {
+    type: "input",
+    name: "emailAdress",
+    message: "What is the intern's email adress?",
+  },
+];
 
 function init() {
-    inquirer.prompt(questions)
-    .then(answers => {
-        if (answers.menu === "Engineer") {
-            inquirer.prompt(engineerQuestions)
-        } else if (answers.menu === "Intern") {
-            inquirer.prompt(internQuestions)
-        }
-    })
+  inquirer.prompt(managerQuestions).then((managerAnswers) => {
+    console.log(managerAnswers);
+    add();
+  });
 }
 
+function add() {
+  inquirer.prompt(addMember).then((addAnswer) => {
+    if (addAnswer.menu === "Engineer") {
+      addEngineer();
+    } else if (addAnswer.menu === "Intern") {
+      addIntern();
+    } //add one for finish building my team
+  });
+}
 
+function addEngineer() {
+  inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
+    console.log(engineerAnswers);
+    add();
+  });
+}
 
-init()
+function addIntern() {
+  inquirer.prompt(internQuestions).then((internAnswers) => {
+    console.log(internAnswers);
+    add();
+  });
+}
 
+init();
